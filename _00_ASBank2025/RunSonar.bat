b6538668303f6cb7cf8590913fdc7fd3838aaec0
Your Name <you@example.com>
2025-11-06 12:18:17 +0100

    mise en place du coverage

--- _00_ASBank2025/RunSonar.bat
+++ _00_ASBank2025/RunSonar.bat
@@ -1,1 +1,33 @@
-mvn -Pcoverage clean verify sonar:sonar -Dsonar.projectKey=BANK_IUT_2023 -Dsonar.organization=Marmelade57_BUT3_QUALDEV_13 -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=sqp_c55e85adf298ef5ffae40c13ec282dfc9482301b -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml
+@echo on
+setlocal
+cd /d "%~dp0"
+
+rem Force le JDK 17 pour Maven (adapter si ton chemin diffère)
+set "JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-17.0.17+10"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
+
+echo Using JAVA_HOME=%JAVA_HOME%
+set "SONAR_PROJECT_KEY=Marmelade57_BUT3_QUALDEV_13"
+set "SONAR_ORG=marmelade57"
+set "SONAR_HOST=https://sonarcloud.io"
+set "SONAR_TOKEN=%SONAR_TOKEN%"
+if "%SONAR_TOKEN%"=="" set "SONAR_TOKEN=ada0520817d2a5383da445e8922b4af80029b032"
+
+echo Sonar params: projectKey=%SONAR_PROJECT_KEY% organization=%SONAR_ORG% host=%SONAR_HOST%
+call mvn -version
+
+rem 1) Build + tests + génération du rapport JaCoCo (ne pas échouer sur tests KO)
+call mvn -B -Pcoverage clean verify -Dmaven.test.failure.ignore=true -DskipTests=false
+if errorlevel 1 goto :eof
+
+rem Vérification présence du rapport
+if not exist target\site\jacoco\jacoco.xml (
+  echo [ERROR] Rapport JaCoCo introuvable: target\site\jacoco\jacoco.xml
+  goto :eof
+)
+
+rem 2) Analyse SonarCloud
+call mvn -B sonar:sonar -Dsonar.projectKey=%SONAR_PROJECT_KEY% -Dsonar.organization=%SONAR_ORG% -Dsonar.host.url=%SONAR_HOST% -Dsonar.login=%SONAR_TOKEN% -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml
+
+endlocal
+@echo off
